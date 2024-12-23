6ae7a82ba9966e5d80f2b74d5f6e9e07f2a55624
avigailll1313 <avigacohen07@gmail.com>
2024-12-23 16:34:05 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+import java.util.*;
+
+public class aaa {
+
+	public static Node<Integer> q(Node<Integer> l1) {
+		Node<Integer> P = l1.getNext();
+		Node<Integer> a = l1;
+		int x = 0;
+		while (l1.hasNext()) {
+			 x= l1.getValue();
+			
+				if (P.getNext().getValue().equals(x)) {
+					P.setNext(P.getNext().getNext());
+				}
+
+			l1.getNext();
+			P.getNext();
+
+		}
+		return (a);
+	}
+
+	public static void main(String arg[]) {
+		int[] a = { 2, 3, 4, 7, 3, 5 };
+		Node<Integer> h = new Node<Integer>(-1);
+		Node<Integer> p = h;
+		Node<Integer> x;
+		for (int i = 0; i < a.length; i++) {
+			x = new Node<Integer>(a[i]);
+			p.setNext(x);
+			p = p.getNext();
+		}
+		
+		System.out.println(h.getNext());
+		System.out.println(q(p));
+	}
+
